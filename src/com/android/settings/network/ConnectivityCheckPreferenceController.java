c451f0b242ba4fa968f3d05b8891bd81f7c193ca
anupritaisno1 <www.anuprita804@gmail.com>
2020-11-25 11:31:02 +0000

    run clang-format
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- src/com/android/settings/network/ConnectivityCheckPreferenceController.java
+++ src/com/android/settings/network/ConnectivityCheckPreferenceController.java
@@ -27,122 +27,133 @@ import android.os.Looper;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
-
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
-import androidx.preference.ListPreference;
-
 import com.android.internal.util.ArrayUtils;
 import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.RestrictedLockUtils.EnforcedAdmin;
 import com.android.settingslib.RestrictedLockUtilsInternal;
-
 import com.android.settingslib.core.lifecycle.events.OnResume;
 
-public class ConnectivityCheckPreferenceController extends BasePreferenceController
-        implements PreferenceControllerMixin, Preference.OnPreferenceChangeListener, OnResume {
+public class ConnectivityCheckPreferenceController
+    extends BasePreferenceController
+    implements PreferenceControllerMixin, Preference.OnPreferenceChangeListener,
+               OnResume {
 
-    private static final String GRAPHENEOS_CAPTIVE_PORTAL_HTTP_URL =
-            "http://connectivitycheck.grapheneos.org/generate_204";
-    private static final String GRAPHENEOS_CAPTIVE_PORTAL_HTTPS_URL =
-            "http://connectivitycheck.grapheneos.org/generate_204";
-    // TODO: maybe in the future we will have proper fallbacks for graphene
-    private static final String GRAPHENEOS_CAPTIVE_PORTAL_FALLBACK_URL =
-            GRAPHENEOS_CAPTIVE_PORTAL_HTTPS_URL;
-    private static final String GRAPHENEOS_CAPTIVE_PORTAL_OTHER_FALLBACK_URL =
-            GRAPHENEOS_CAPTIVE_PORTAL_HTTPS_URL;
+  private static final String GRAPHENEOS_CAPTIVE_PORTAL_HTTP_URL =
+      "http://connectivitycheck.grapheneos.org/generate_204";
+  private static final String GRAPHENEOS_CAPTIVE_PORTAL_HTTPS_URL =
+      "http://connectivitycheck.grapheneos.org/generate_204";
+  // TODO: maybe in the future we will have proper fallbacks for graphene
+  private static final String GRAPHENEOS_CAPTIVE_PORTAL_FALLBACK_URL =
+      GRAPHENEOS_CAPTIVE_PORTAL_HTTPS_URL;
+  private static final String GRAPHENEOS_CAPTIVE_PORTAL_OTHER_FALLBACK_URL =
+      GRAPHENEOS_CAPTIVE_PORTAL_HTTPS_URL;
 
-    private static final int GRAPHENEOS_CAPTIVE_PORTAL_HTTP_URL_INTVAL = 0;
-    private static final int STANDARD_CAPTIVE_PORTAL_HTTP_URL_INTVAL = 1;
+  private static final int GRAPHENEOS_CAPTIVE_PORTAL_HTTP_URL_INTVAL = 0;
+  private static final int STANDARD_CAPTIVE_PORTAL_HTTP_URL_INTVAL = 1;
 
-    // imported defaults from networkstack
-    private static final String DEFAULT_HTTPS_URL = "https://www.google.com/generate_204";
-    private static final String DEFAULT_HTTP_URL =
-            "http://connectivitycheck.gstatic.com/generate_204";
-    private static final String DEFAULT_FALLBACK_URL  = "http://www.google.com/gen_204";
-    private static final String DEFAULT_OTHER_FALLBACK_URLS =
-            "http://play.googleapis.com/generate_204";
+  // imported defaults from networkstack
+  private static final String DEFAULT_HTTPS_URL =
+      "https://www.google.com/generate_204";
+  private static final String DEFAULT_HTTP_URL =
+      "http://connectivitycheck.gstatic.com/generate_204";
+  private static final String DEFAULT_FALLBACK_URL =
+      "http://www.google.com/gen_204";
+  private static final String DEFAULT_OTHER_FALLBACK_URLS =
+      "http://play.googleapis.com/generate_204";
 
-    private static final String KEY_CONNECTIVITY_CHECK_SETTINGS = "connectivity_check_settings";
+  private static final String KEY_CONNECTIVITY_CHECK_SETTINGS =
+      "connectivity_check_settings";
 
-    private ListPreference mConnectivityPreference;
+  private ListPreference mConnectivityPreference;
 
-    public ConnectivityCheckPreferenceController(Context context) {
-        super(context, KEY_CONNECTIVITY_CHECK_SETTINGS);
+  public ConnectivityCheckPreferenceController(Context context) {
+    super(context, KEY_CONNECTIVITY_CHECK_SETTINGS);
+  }
+
+  @Override
+  public int getAvailabilityStatus() {
+    return BasePreferenceController.AVAILABLE;
+  }
+
+  @Override
+  public void displayPreference(PreferenceScreen screen) {
+    super.displayPreference(screen);
+    mConnectivityPreference =
+        screen.findPreference(KEY_CONNECTIVITY_CHECK_SETTINGS);
+    updatePreferenceState();
+  }
+
+  @Override
+  public String getPreferenceKey() {
+    return KEY_CONNECTIVITY_CHECK_SETTINGS;
+  }
+
+  private void updatePreferenceState() {
+    switch (
+        Settings.Global.getString(mContext.getContentResolver(),
+                                  Settings.Global.CAPTIVE_PORTAL_HTTP_URL)) {
+    case GRAPHENEOS_CAPTIVE_PORTAL_HTTP_URL:
+      mConnectivityPreference.setValueIndex(
+          GRAPHENEOS_CAPTIVE_PORTAL_HTTP_URL_INTVAL);
+      break;
+    case DEFAULT_HTTP_URL:
+      mConnectivityPreference.setValueIndex(
+          STANDARD_CAPTIVE_PORTAL_HTTP_URL_INTVAL);
+      break;
+    default:
+      mConnectivityPreference.setValueIndex(
+          GRAPHENEOS_CAPTIVE_PORTAL_HTTP_URL_INTVAL);
     }
+  }
 
-    @Override
-    public int getAvailabilityStatus() {
-        return BasePreferenceController.AVAILABLE;
-    }
+  @Override
+  public void onResume() {
+    updatePreferenceState();
+    if (mConnectivityPreference != null)
+      setCaptivePortalURLs(
+          mContext.getContentResolver,
+          Integer.parseInt(mConnectivityPreference.getValue()));
+  }
 
-    @Override
-    public void displayPreference(PreferenceScreen screen) {
-        super.displayPreference(screen);
-        mConnectivityPreference = screen.findPreference(KEY_CONNECTIVITY_CHECK_SETTINGS);
-        updatePreferenceState();
+  private void setCaptivePortalURLs(ContentResolver cr, int mode) {
+    switch (mode) {
+    case STANDARD_CAPTIVE_PORTAL_HTTP_URL_INTVAL:
+      Settings.Global.putString(cr, Settings.Global.CAPTIVE_PORTAL_HTTP_URL,
+                                DEFAULT_HTTP_URL);
+      Settings.Global.putString(cr, Settings.Global.CAPTIVE_PORTAL_HTTPS_URL,
+                                DEFAULT_HTTPS_URL);
+      Settings.Global.putString(cr, Settings.Global.CAPTIVE_PORTAL_FALLBACK_URL,
+                                DEFAULT_FALLBACK_URL);
+      Settings.Global.putString(
+          cr, Settings.Global.CAPTIVE_PORTAL_OTHER_FALLBACK_URLS,
+          DEFAULT_OTHER_FALLBACK_URLS);
+      break;
+    // intentional fallthrough
+    case GRAPHENEOS_CAPTIVE_PORTAL_HTTP_URL_INTVAL:
+    default:
+      Settings.Global.putString(cr, Settings.Global.CAPTIVE_PORTAL_HTTP_URL,
+                                GRAPHENEOS_CAPTIVE_PORTAL_HTTP_URL);
+      Settings.Global.putString(cr, Settings.Global.CAPTIVE_PORTAL_HTTPS_URL,
+                                GRAPHENEOS_CAPTIVE_PORTAL_HTTPS_URL);
+      Settings.Global.putString(cr, Settings.Global.CAPTIVE_PORTAL_FALLBACK_URL,
+                                GRAPHENEOS_CAPTIVE_PORTAL_FALLBACK_URL);
+      Settings.Global.putString(
+          cr, Settings.Global.CAPTIVE_PORTAL_OTHER_FALLBACK_URLS,
+          GRAPHENEOS_CAPTIVE_PORTAL_OTHER_FALLBACK_URL);
     }
+  }
 
-    @Override
-    public String getPreferenceKey() {
-        return KEY_CONNECTIVITY_CHECK_SETTINGS;
-    }
-
-    private void updatePreferenceState() {
-        switch(Settings.Global.getString(mContext.getContentResolver(),
-                Settings.Global.CAPTIVE_PORTAL_HTTP_URL)) {
-            case GRAPHENEOS_CAPTIVE_PORTAL_HTTP_URL:
-                mConnectivityPreference.setValueIndex(GRAPHENEOS_CAPTIVE_PORTAL_HTTP_URL_INTVAL);
-		break;
-            case DEFAULT_HTTP_URL:
-                mConnectivityPreference.setValueIndex(STANDARD_CAPTIVE_PORTAL_HTTP_URL_INTVAL);
-		break;
-            default:
-                mConnectivityPreference.setValueIndex(GRAPHENEOS_CAPTIVE_PORTAL_HTTP_URL_INTVAL);
-         }
-    }
-
-    @Override
-    public void onResume() {
-        updatePreferenceState();
-        if (mConnectivityPreference != null)
-	    setCaptivePortalURLs(mContext.getContentResolver, Integer.parseInt(mConnectivityPreference.getValue()));
-    }
-
-    private void setCaptivePortalURLs(ContentResolver cr, int mode) {
-        switch(mode) {
-            case STANDARD_CAPTIVE_PORTAL_HTTP_URL_INTVAL:
-            Settings.Global.putString(cr, Settings.Global.CAPTIVE_PORTAL_HTTP_URL,
-                    DEFAULT_HTTP_URL);
-            Settings.Global.putString(cr, Settings.Global.CAPTIVE_PORTAL_HTTPS_URL,
-                    DEFAULT_HTTPS_URL);
-            Settings.Global.putString(cr, Settings.Global.CAPTIVE_PORTAL_FALLBACK_URL,
-                    DEFAULT_FALLBACK_URL);
-            Settings.Global.putString(cr, Settings.Global.CAPTIVE_PORTAL_OTHER_FALLBACK_URLS,
-                    DEFAULT_OTHER_FALLBACK_URLS);
-	    break;
-	    // intentional fallthrough
-            case GRAPHENEOS_CAPTIVE_PORTAL_HTTP_URL_INTVAL:
-	    default:
-            Settings.Global.putString(cr, Settings.Global.CAPTIVE_PORTAL_HTTP_URL,
-                    GRAPHENEOS_CAPTIVE_PORTAL_HTTP_URL);
-            Settings.Global.putString(cr, Settings.Global.CAPTIVE_PORTAL_HTTPS_URL,
-                    GRAPHENEOS_CAPTIVE_PORTAL_HTTPS_URL);
-            Settings.Global.putString(cr, Settings.Global.CAPTIVE_PORTAL_FALLBACK_URL,
-                    GRAPHENEOS_CAPTIVE_PORTAL_FALLBACK_URL);
-            Settings.Global.putString(cr, Settings.Global.CAPTIVE_PORTAL_OTHER_FALLBACK_URLS,
-                    GRAPHENEOS_CAPTIVE_PORTAL_OTHER_FALLBACK_URL);
-        }
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object value) {
-        final String key = preference.getKey();
-        if (KEY_CONNECTIVITY_CHECK_SETTINGS.equals(key))
-            return true;
-        else
-            return false;
-    }
+  @Override
+  public boolean onPreferenceChange(Preference preference, Object value) {
+    final String key = preference.getKey();
+    if (KEY_CONNECTIVITY_CHECK_SETTINGS.equals(key))
+      return true;
+    else
+      return false;
+  }
 }
