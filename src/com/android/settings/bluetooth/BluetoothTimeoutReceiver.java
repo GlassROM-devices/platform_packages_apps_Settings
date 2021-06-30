3c4f4fb08a8cfc0adf460634178fabd24547bca2
anupritaisno1 <www.anuprita804@gmail.com>
2021-06-30 09:12:46 +0530

    fixes
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>
    Change-Id: I54da61b26f166ba0f986462649b4208884eac05d

--- src/com/android/settings/bluetooth/BluetoothTimeoutReceiver.java
+++ src/com/android/settings/bluetooth/BluetoothTimeoutReceiver.java
@@ -11,78 +11,76 @@ import android.util.Log;
 import java.util.Date;
 
 public class BluetoothTimeoutReceiver extends BroadcastReceiver {
-	private static final String TAG = "BluetoothTimeoutReceiver";
-	private static final boolean DEBUG = false;
+    private static final String TAG = "BluetoothTimeoutReceiver";
+    private static final boolean DEBUG = false;
 
-	// The intents
-	private static final String TIMEOUT_INTENT =
-		"android.bluetooth.intent.TIMEOUT";
-	private static final String BLUETOOTH_DISCONNECTED_INTENT =
-		"android.bluetooth.device.action.ACL_DISCONNECTED";
-	private static final String BLUETOOTH_CONNECTED_INTENT =
-		"android.bluetooth.device.action.ACL_CONNECTED";
+    // The intents
+    private static final String TIMEOUT_INTENT =
+        "android.bluetooth.intent.TIMEOUT";
+    private static final String BLUETOOTH_DISCONNECTED_INTENT =
+        "android.bluetooth.device.action.ACL_DISCONNECTED";
+    private static final String BLUETOOTH_CONNECTED_INTENT =
+        "android.bluetooth.device.action.ACL_CONNECTED";
 
-	// a variable we use to store our intent
-	private static PendingIntent mPendingIntent;
+    // a variable we use to store our intent
+    private static PendingIntent mPendingIntent;
 
-	public static void setTimeout(int btTimeout) {
-		long when = SystemClock.elapsedRealtime() + btTimeout;
-		Intent btIntent = new Intent(TIMEOUT_INTENT);
-		btIntent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
-		PendingIntent sender = PendingIntent.getBroadcast(
-				mContext, 0, btIntent, PendingIntent.FLAG_CANCEL_CURRENT);
-		mAlarmManager.setExactAndAllowWhileIdle(
-				AlarmManager.ELAPSED_REALTIME_WAKEUP, when, sender);
-	}
+    public static void setTimeout(int btTimeout) {
+        long when = SystemClock.elapsedRealtime() + btTimeout;
+        Intent btIntent = new Intent(TIMEOUT_INTENT);
+        btIntent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+        PendingIntent sender = PendingIntent.getBroadcast(
+                mContext, 0, btIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        mAlarmManager.setExactAndAllowWhileIdle(
+                AlarmManager.ELAPSED_REALTIME_WAKEUP, when, sender);
+    }
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		String intentAction = intent.getAction();
-		BluetoothAdaper bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-		if (bluetoothAdapter == null) {
-			if (DEBUG) Log.e(TAG, "Bluetooth Adapter is null!");
-			return;
-		}
-		// TODO: is this needed?
-		if (intentAction == null) {
-			if (DEBUG) Log.e(TAG, "Received an intent with a null action");
-			return;
-		}
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String intentAction = intent.getAction();
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            if (DEBUG) Log.e(TAG, "Bluetooth Adapter is null!");
+            return;
+        }
 
-		if (TIMEOUT_INTENT.equals(intent.getAction())) {
-			if (shouldDisableBluetooth() && isBluetoothEnabledAndNotConnected(bluetoothAdapter)) {
-				bluetoothAdapter.disable();
-			}
-                }
+        // TODO: is this needed?
+        if (intentAction == null) {
+            if (DEBUG) Log.e(TAG, "Received an intent with a null action");
+            return;
+        }
 
-		else if (BLUETOOTH_DISCONNECTED_INTENT.equals(intent.getAction())) {
-			if (shouldDisableBluetooth()) {
-				setTimeout(
-						Settings.Global.getLong(context.getContentResolver(),
-							Settings.Global.BLUETOOTH_OFF_TIMEOUT));
-			}
-		}
+        if (TIMEOUT_INTENT.equals(intent.getAction())) {
+            if (shouldDisableBluetooth() && isBluetoothEnabledAndNotConnected(bluetoothAdapter)) {
+                bluetoothAdapter.disable();
+            }
+        }
 
-		else if(BLUETOOTH_CONNECTED_INTENT.equals(intent.getAction())) {
-			if (!shouldDisableBluetooth() || mPendingIntent == null) {
-				// don't bother
-				return;
-			}
+        else if (BLUETOOTH_DISCONNECTED_INTENT.equals(intent.getAction())) {
+            if (shouldDisableBluetooth() && isBluetoothEnabledAndNotConnected(bluetoothAdapter)) {
+                setTimeout(Settings.Global.getLong(context.getContentResolver(),
+                            Settings.Global.BLUETOOTH_OFF_TIMEOUT));
+            }
+        }
 
-			context.getSystemService(Context.ALARM_SERVICE).cancel(mPendingIntent);
-			mPendingIntent.cancel();
+        else if(BLUETOOTH_CONNECTED_INTENT.equals(intent.getAction())) {
+            if (!shouldDisableBluetooth() || mPendingIntent == null) {
+                // don't bother
+                return;
+            }
+            context.getSystemService(context.ALARM_SERVICE).cancel(mPendingIntent);
+            mPendingIntent.cancel();
+        }
+    }
 
-		}
-	}
+    private static boolean shouldDisableBluetooth() {
+        return (Settings.Global.getLong(mContext.getContentResolver(),
+                    Settings.Global.BLUETOOTH_OFF_TIMEOUT, 0)) > 0;
+    }
 
-	private static boolean shouldDisableBluetooth() {
-		return (Settings.Global.getLong(context.getContentResolver(),
-					Settings.Global.BLUETOOTH_OFF_TIMEOUT, 0)) > 0;
-	}
-
-	private static boolean isBluetoothEnabledAndNotConnected(BluetoothAdaper bluetoothAdapter) {
-		return isBluetoothEnabled(bluetoothAdapter) &&
-			!isBluetoothConnected(bluetoothAdapter) &&
-			!bluetoothAdapter.isDiscovering();
-	}
+    private static boolean isBluetoothEnabledAndNotConnected(BluetoothAdapter bluetoothAdapter) {
+        return isBluetoothEnabled(bluetoothAdapter) &&
+            !isBluetoothConnected(bluetoothAdapter) &&
+            !bluetoothAdapter.isDiscovering();
+    }
 }
